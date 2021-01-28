d465ca01cd40427137ff917a1c7a46d067f87bc1
gsanyas <guillem.sanyas@imt-atlantique.net>
2021-01-28 20:36:41 +0100

    add communication

--- mainInterface/index.js
+++ mainInterface/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
-
+const axios = require('axios');
 const app = express();
 
-app.get("/", (req, res) => res.send("Ok"));
+app.get("/", (req, res) => {
+
+    let result = await axios.get('http://localhost:8001/');
+    res.send(result)});
 
 app.listen(8000, () => {
     console.log("Server started !");
