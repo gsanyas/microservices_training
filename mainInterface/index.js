44ae0b7c59332677e5bdba03317f3f489db05eab
gsanyas <guillem.sanyas@imt-atlantique.net>
2021-01-28 20:46:55 +0100

    debug

--- mainInterface/index.js
+++ mainInterface/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const axios = require('axios');
+const axios = require("axios");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json())
 
 app.get("/", async (req, res) => {
-
-    let result = await axios.get('http://localhost:8001/');
-    res.send(result)});
+    let result = await axios.get("http://localhost:8001/");
+    console.log(result.data)
+    res.send(result.data);
+});
 
 app.listen(8000, () => {
     console.log("Server started !");
